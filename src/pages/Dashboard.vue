2f42675f2ae01659f0a5f443cea9c20a57e2a8ac
khursandov <xursandov_s@mail.ru>
2019-12-26 02:27:06 +0500

    init app

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+
+    <!--Stats cards-->
+    <div class="row">
+      <div class="col-md-6 col-xl-3" v-for="stats in statsCards" :key="stats.title">
+        <stats-card>
+          <div class="icon-big text-center" :class="`icon-${stats.type}`" slot="header">
+            <i :class="stats.icon"></i>
+          </div>
+          <div class="numbers" slot="content">
+            <p>{{stats.title}}</p>
+            {{stats.value}}
+          </div>
+          <div class="stats" slot="footer">
+            <i :class="stats.footerIcon"></i> {{stats.footerText}}
+          </div>
+        </stats-card>
+      </div>
+    </div>
+
+    <!--Charts-->
+
+
+  </div>
+</template>
+<script>
+import { StatsCard, ChartCard } from "@/components/index";
+import Chartist from 'chartist';
+export default {
+  components: {
+    StatsCard,
+    ChartCard
+  },
+  /**
+   * Chart data used to render stats, charts. Should be replaced with server data
+   */
+  data() {
+    return {
+      statsCards: [
+        {
+          type: "info",
+          icon: "ti-server",
+          title: "On proccess",
+          value: "2",
+          footerText: "Updated now",
+          footerIcon: "ti-reload"
+        },
+        {
+          type: "danger",
+          icon: "ti-pulse",
+          title: "Paused",
+          value: "23",
+          footerText: "In the last hour",
+          footerIcon: "ti-timer"
+        },
+        {
+          type: "success",
+          icon: "ti-server",
+          title: "This month",
+          value: "45",
+          footerText: "Updated now",
+          footerIcon: "ti-reload"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style>
+</style>
